42488e0147f90117ee271e15f7b6e63892d3909b
Giuseppe Bruno <giuseppe.bruno@finconsgroup.com>
2026-07-17 10:27:36 +0200

    Initial project setup

---
+++ src/main/java/it/fincons/reservation_manager_rest_api/ReservationManagerRestApiApplication.java
@@ -0,0 +1,13 @@
+package it.fincons.reservation_manager_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationManagerRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationManagerRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/fincons/reservation_manager_rest_api/ReservationManagerRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.fincons.reservation_manager_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationManagerRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
